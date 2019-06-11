8c6879ae7d8044ff42a90da4ef9a53ad36b996e5
J-Agens <johntagens@gmail.com>
2019-06-11 17:42:37 -0400

    made a logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const logOut = () => {
+    localStorage.clear();
+    // use dispatch to logout
+  }
   return (
     <div className="navbar">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/table">Table</NavLink>
       <NavLink to="/main">Main</NavLink>
       <NavLink to="/kitchen">Kitchen</NavLink>
+      <button onClick={logOut}>Log Out</button>
     </div>
   );
 };
