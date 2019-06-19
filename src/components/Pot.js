1a9caebc98aeeee04a5cd9cb935d3dd8da5dfbeb
J-Agens <johntagens@gmail.com>
2019-06-19 17:49:35 -0400

    added progress bar

--- src/components/Pot.js
+++ src/components/Pot.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUtensilSpoon, faStopCircle } from '@fortawesome/free-solid-svg-icons'
-
+import ProgressBar from 'react-bootstrap/ProgressBar'
 
 class Pot extends Component {
 
@@ -22,9 +22,10 @@ class Pot extends Component {
   }
 
   renderIngs = () => {
-    return this.state.ingredients.map((ing, idx) => {
-      return <span key={idx} className="tiny">{ing.slice(0, 3)}|</span>
-    });
+    // return this.state.ingredients.map((ing, idx) => {
+    //   return <span key={idx} className="tiny">{ing.slice(0, 3)}|</span>
+    // });
+    return <ProgressBar animated now={(this.state.ingredients.length / 4) * 100}/>
   }
 
   render() {
@@ -32,16 +33,17 @@ class Pot extends Component {
       <div className="pot col-2" style={this.props.selectedCookSession && this.props.selectedCookSession.pot_id === this.props.pot.id ? {backgroundImage: "linear-gradient(#969696, #49E7A5)"} : null} onDragOver={(e) => this.props.onDragOver(e)} onDrop={this.handleOnDrop}>
 
         <h6>
+        { this.props.selectedCookSession && this.props.selectedCookSession.pot_id === this.props.pot.id ? this.renderIngs() :
         <p style={{fontSize: ".75em", paddingTop: "5%"}}>
         {this.props.pot.cook_session ? this.props.pot.cook_session.recipe_name + " - ": null}
         { this.props.pot.cook_session ? this.props.pot.cook_session.customer_name : null}
         </p>
+        }
         <div>
           {this.props.pot.cook_session ? <button className="btn cancel-cook watermark pot-watermark"onClick={() => this.props.deleteCookSession(this.props.pot.cook_session.id, this.props.pot.id)}><FontAwesomeIcon icon={faStopCircle} size="2x"/></button> : null}
           {this.props.pot.cook_session ? <button className="btn select-cook watermark pot-watermark" onClick={() => this.props.selectCookSession(this.props.pot.cook_session, this.state.ingredients)}><FontAwesomeIcon icon={faUtensilSpoon} size="2x"/></button> : null}
         </div>
         </h6>
-        { this.renderIngs() }
       </div>
     )
   }
